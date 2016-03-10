bc5753720e15913cc773267b105c3cb334d829ce
Egor Blinov <escaton@yandex-team.ru>
2016-03-11 02:04:27 +0300

    initial support mods

--- src/pages/bem-sidebar/blocks/sidebar/sidebar.jsx
+++ src/pages/bem-sidebar/blocks/sidebar/sidebar.jsx
@@ -1,18 +1,57 @@
 'use strict';
 
 function getBlocks() {
-    var el = $0;
-    var classes = Array.prototype.slice.call($0.classList, 0);
-    var res = [];
-    if (!~classes.indexOf('i-bem')) {
+
+    function extractMods(elem, name) {
+        var res = {};
+        var MOD_DELIM = BEM.INTERNAL.MOD_DELIM;
+        var NAME_PATTERN = BEM.INTERNAL.NAME_PATTERN;
+        var regexp = new RegExp([
+                '(\\s|^)',
+                name,
+                MOD_DELIM,
+                '(',
+                NAME_PATTERN,
+                ')',
+                MOD_DELIM,
+                '(',
+                NAME_PATTERN,
+                ')(?=\\s|$)'
+            ].join(''), 'g');
+
+        (elem.className.match(regexp) || []).forEach(function(className) {
+            var iModVal = (className = className.trim()).lastIndexOf(MOD_DELIM),
+                iModName = className.substr(0, iModVal - 1).lastIndexOf(MOD_DELIM);
+            res[className.substr(iModName + 1, iModVal - iModName - 1)] = className.substr(iModVal + 1);
+        });
         return res;
     }
+
+    var el = $0;
+    var blockRegex = new RegExp('(\\s|^)' + BEM.INTERNAL.NAME_PATTERN + '(?=\\s|$)');
+    var classes = Array.prototype.slice.call(el.classList, 0);
+    var res = {};
+    classes
+        .forEach((className) => {
+            if (className === 'i-bem') {
+                return;
+            }
+            if (blockRegex.test(className)) {
+                var mods = extractMods(el, className);
+                res[className] = {
+                    mods: mods
+                }
+            }
+        });
     var bemData = el.dataset.bem;
-    if (!bemData) {
-        return [];
+    if (bemData) {
+        var jsData = JSON.parse(bemData);
+        Object.keys(jsData).forEach((block) => {
+            res[block] = res[block] || {};
+            res[block].params = jsData[block];
+        });
     }
-    var jsData = JSON.parse(bemData);
-    return jsData;
+    return res;
 }
 
 import React from 'react';
