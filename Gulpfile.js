79a6d8c212e9d050721e49a16c765d8dc2693069
Egor Blinov <escaton@yandex-team.ru>
2016-03-11 17:59:42 +0300

    watch task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,24 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var glob = require('glob');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var es = require('event-stream');
+var minimist = require('minimist');
+var options = minimist(process.argv.slice(2));
+var production = process.env.NODE_ENV === 'production';
+
+var WATCH = false;
+
+gulp.task('enable-watch', function() {
+    WATCH = true;
+})
+
+gulp.task('watch', ['enable-watch', 'build']);
 
 gulp.task('build', function(done) {
     glob('./src/pages/*/*.entry.js', function(err, files) {
@@ -14,16 +27,42 @@ gulp.task('build', function(done) {
             return;
         }
         var tasks = files.map(function(entry) {
-            return browserify({
-                    entries: [entry]
-                })
-                .transform('babelify', {presets: ['es2015', 'react']})
-                .bundle()
-                .pipe(source(entry))
-                .pipe(rename(function(path) {
-                    path.basename = path.basename.replace(/entry$/, 'bundle')
-                }))
-                .pipe(gulp.dest('./'));
+            var bundler = browserify({
+                    entries: [entry],
+                    debug: !production,
+                    cache: {}, // required for watchify
+                    packageCache: {}, // required for watchify
+                    fullPaths: WATCH // required to be true only for watchify
+                });
+
+            if (WATCH) {
+                bundler.plugin(watchify, {
+                    delay: 100,
+                    ignoreWatch: ['**/node_modules/**'],
+                    poll: true
+                });
+            }
+
+            bundler.transform('babelify', {presets: ['es2015', 'react']});
+
+            var rebundle = function() {
+                gutil.log('Start rebuild');
+                var stream = bundler
+                    .bundle()
+                    .pipe(source(entry))
+                    .pipe(rename(function(path) {
+                        path.basename = path.basename.replace(/entry$/, 'bundle')
+                    }))
+                    .pipe(gulp.dest('./'));
+
+                stream.on('error', gutil.log);
+                return stream;
+            };
+
+            bundler.on('log', gutil.log);
+            bundler.on('update', rebundle);
+            return rebundle();
+
         });
         es.merge(tasks).on('end', done);
     });
