61f9280710f02f4eb7f102fb989e71086595f0b9
Egor Blinov <escaton@yandex-team.ru>
2016-03-11 17:59:25 +0300

    rewrite tree-inspect

--- src/pages/bem-sidebar/blocks/tree-inspect/tree-inspect.jsx
+++ src/pages/bem-sidebar/blocks/tree-inspect/tree-inspect.jsx
@@ -6,64 +6,68 @@ class TreeInspect extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            collapsed: this.props.collapsed
+            collapsed: {}
         }
     }
-    onCollapseToggle() {
+    onCollapseToggle(name) {
+        var collapsed = this.state.collapsed;
+        collapsed[name] = !collapsed[name];
         this.setState({
-            collapsed: !this.state.collapsed
+            collapsed: collapsed
         });
     }
     render() {
         var self = this;
         var data = self.props.data || {};
-        var label = self.props.label;
-        if (label) {
-            if (Object.keys(data).length) {
-                return (
-                    <li className="tree-inspect__item tree-inspect__item_type_object">
-                        <span className="tree-inspect__label">
-                            <i className={"tree-inspect__collapse-toggle  tree-inspect__collapse-toggle_collapsed_" + (self.state.collapsed ? 'yes' : 'no')} onClick={self.onCollapseToggle.bind(self)}></i>
-                            {label}
-                        </span>
-                        <div className={"tree-inspect__inner tree-inspect__inner_collapsed_" + (self.state.collapsed ? 'yes' : 'no')}>
-                            <TreeInspect data={data}/>
-                        </div>
-                    </li>
-                )
-            } else {
-                return (
-                    <li className="tree-inspect__item">
-                        <span className="tree-inspect__label tree-inspect__label_type_primitive">{label}</span>
-                        <span className="tree-inspect__value">{"{}"}</span>
-                    </li>
-                )
-            }
-        } else {
-            var nodes = Object.keys(data).map((node, index) => {
-                var value = data[node];
-                if (typeof value === 'object') {
-                    return (
-                        <TreeInspect data={value} label={node}/>
-                    )
+
+        var nodes = Object.keys(data).map((node, index) => {
+            var value = data[node];
+            var content;
+            var needCollapseToggle = false;
+            var isCollapsed = self.state.collapsed[node];
+            var isPrimitive = true;
+            if (typeof value === 'object') {
+                if (value === null) {
+                    content = "null";
+                } else if (Object.keys(value).length) {
+                    needCollapseToggle = true;
+                    isPrimitive = false;
+                    content = <TreeInspect data={value}/>
                 } else {
-                    return (
-                        <li className="tree-inspect__item">
-                            <span className="tree-inspect__label tree-inspect__label_type_primitive">{node}</span>
-                            <span className="tree-inspect__value">{JSON.stringify(value)}</span>
-                        </li>
-                    )
+                    content = "{}";
                 }
-            });
-            if (nodes.length) {
-                return (
-                    <ul className="tree-inspect">
-                        {nodes}
-                    </ul>
-                )
             } else {
-                return false
+                content = JSON.stringify(value);
             }
+            return (
+                <li className={
+                    'tree-inspect__item' +
+                    ' tree-inspect__item_type_' + typeof(value) +
+                    ' tree-inspect__item_collapsable_' + (needCollapseToggle ? 'yes' : 'no') +
+                    ' tree-inspect__item_primitive_' + (isPrimitive ? 'yes' : 'no')
+                }>
+                    <span className="tree-inspect__label">
+                        { needCollapseToggle
+                            ? <i className={
+                                'tree-inspect__collapse-toggle' +
+                                ' tree-inspect__collapse-toggle_collapsed_' + (isCollapsed ? 'yes' : 'no')
+                                } onClick={self.onCollapseToggle.bind(self, node)}></i>
+                            : false
+                        }
+                        {node}
+                    </span>
+                    <span className={"tree-inspect__value tree-inspect__value_collapsed_" + (isCollapsed ? 'yes' : 'no')}>{content}</span>
+                </li>
+            );
+        });
+        if (nodes.length) {
+            return (
+                <ul className="tree-inspect">
+                    {nodes}
+                </ul>
+            )
+        } else {
+            return false;
         }
     }
 }
